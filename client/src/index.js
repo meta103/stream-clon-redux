5f4573db98cdbcd72310c44524612992fec626f4
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-04-18 19:13:29 +0200

    redux-devtools-extension setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+//applyMiddleware & compose son para la extension de chrome redux-devtools-extension
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+//SETUP PARA redux-devtools-extension (optional)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware())
+);
+
+// const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
