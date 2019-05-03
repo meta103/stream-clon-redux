dc74cfa854c8108ad590e1a62ea7bc1f83c3b7b7
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-05-03 12:21:44 +0200

    Fetching a Stream for Edit Stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = props => {
-  console.log(props)
-  return (
-    <div>
-      StreamEdit
-    </div>
-  )
-};
+class StreamEdit extends Component {
+  //Este paso es importante para hacer isolacion: llamamos a la info de la API desde aqui tambien, no solo desde la home page.
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return <div> {this.props.stream.title} </div>
+  }
+}
+
 
 //El segundo parametro, ownProps, hace referencia a las props que vienen dadas por el router dom (History, location, match, etc)
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
